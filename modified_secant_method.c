5dd73690e415e339ccc6de89b8e2c59592458802
Shafin6657 <shafinmahmud893@gmail.com>
2025-09-20 00:49:40 +0600

    project

--- modified_secant_method.c
+++ modified_secant_method.c
@@ -1,4 +1,18 @@
 //shimanto
+// SHIMANTO
+
+
+// Section 1: Header Files and Function Definition
+// This section includes necessary libraries and defines the function f(x)
+// whose root we want to find.
+#include <stdio.h>
+#include <math.h>
+
+// Test function: f(x) = e^x - 3x
+double f(double x) {
+    return exp(x) - 3*x;
+}
+
 //Rafiza
 //RAtul
 //rifaft
